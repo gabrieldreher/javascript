daa07a028ea6a5d0606742c095c300ad35b7ed53
gabrieldreher <147192328+gabrieldreher@users.noreply.github.com>
2023-11-22 11:20:02 -0300

    Criei ex008.js, ex009.js e ex10.html

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.1
+console.log(`A velocidade do seu carro é de: ${vel}km/h`)
+    if (vel > 60){ //Condição simples
+        console.log(`Você ultrapassou a velocidade permitida e foi multado.`)
+    }
+
+console.log(`Dirija sempre usando cinto de segurança!`)
