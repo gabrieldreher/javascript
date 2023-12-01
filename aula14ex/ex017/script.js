c2ef4f49ac3e2faadd01fd4649c10194da3ed39a
gabrieldreher <147192328+gabrieldreher@users.noreply.github.com>
2023-12-01 14:31:15 -0300

    Up ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -6,22 +6,29 @@ var passos = document.getElementById('passos')
 var res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = `Impossível contar`
+
     }else {
-        res.innerHTML = `Contando:`
+        res.innerHTML = `Contando: <br>`
         var inicio = Number(inicio.value)
         var fim = Number(fim.value)
         var passos = Number(passos.value)
+        if (passos <= 0){
+            window.alert(`Passo inválido, considerando Passo 1`)
+            passos = 1
+        }
 
         if(inicio < fim) {
+            //contagem crescente
             for(var c = inicio; c <= fim; c+= passos) {
                 res.innerHTML += ` ${c} \u{1F449} `
             }
-            res.innerHTML += `\u{1f3c1}`
+            //contagem regressiva
         }else {
-            
+            for(var c = inicio; c >= fim; c -= passos)
+                res.innerHTML += ` ${c} \u{1F449} `
         }
-
+            res.innerHTML += `\u{1f3c1}`
         
 
     }
